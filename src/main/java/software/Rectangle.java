8e809bf888bdd388c89954992353cf3ef8d3adc6
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-07-18 12:15:42 +0330

    Pass test

--- src/main/java/software/Rectangle.java
+++ src/main/java/software/Rectangle.java
@@ -1,11 +1,15 @@
 package software;
 
 public class Rectangle {
+    private final int width;
+    private final int height;
 
     public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 
     public int calculateArea() {
-        return -1;
+        return this.width * this.height;
     }
 }
